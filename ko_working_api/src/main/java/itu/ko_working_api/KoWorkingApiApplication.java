7f9a1815df7e3622ca1985474274be352b5e63eb
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-08 15:09:05 +0300

    [init]-Add spring project

---
+++ ko_working_api/src/main/java/itu/ko_working_api/KoWorkingApiApplication.java
@@ -0,0 +1,13 @@
+package itu.ko_working_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoWorkingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoWorkingApiApplication.class, args);
+    }
+
+}
---
+++ ko_working_api/src/test/java/itu/ko_working_api/KoWorkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.ko_working_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoWorkingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
