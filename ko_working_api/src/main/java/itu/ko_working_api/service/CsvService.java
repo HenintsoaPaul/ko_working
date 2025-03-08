7381fd87d4bb2e10bf40c6f7a754270809ee5169
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-08 17:17:50 +0300

    [csv][api]-Add csv mapper

---
+++ ko_working_api/src/main/java/itu/ko_working_api/service/CsvService.java
@@ -0,0 +1,31 @@
+package itu.ko_working_api.service;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import itu.ko_working_api.dto.upload.EspaceUpload;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Service
+public class CsvService {
+
+    public List<EspaceUpload> parseEspaceUpload(MultipartFile file) throws IOException {
+        try (
+                InputStreamReader isr = new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+        ) {
+            CsvToBean<EspaceUpload> csvToBean = new CsvToBeanBuilder<EspaceUpload>(br)
+                    .withType(EspaceUpload.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            return csvToBean.parse();
+        }
+    }
+}
