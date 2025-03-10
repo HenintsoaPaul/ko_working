e645e9876a3b9edcabcc38f627ee1695f13febab
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-10 03:08:18 +0300

    [bdd]-Add sequences for option and prix_option

--- ko_working_api/database/seq.sql
+++ ko_working_api/database/seq.sql
@@ -23,3 +23,29 @@ BEGIN
     RETURN 'PRIX_ESPACE' || LPAD(next_val::TEXT, 4, '0');
 END;
 $$ LANGUAGE plpgsql;
+
+-- option
+CREATE SEQUENCE option_seq START 1 INCREMENT 1;
+
+CREATE OR REPLACE FUNCTION generate_option_seq()
+    RETURNS TEXT AS $$
+DECLARE
+    next_val INT;
+BEGIN
+    SELECT nextval('option_seq') INTO next_val;
+    RETURN 'OPT' || LPAD(next_val::TEXT, 4, '0');
+END;
+$$ LANGUAGE plpgsql;
+
+-- prix_option
+CREATE SEQUENCE prix_option_seq START 1 INCREMENT 1;
+
+CREATE OR REPLACE FUNCTION generate_prix_option_seq()
+    RETURNS TEXT AS $$
+DECLARE
+    next_val INT;
+BEGIN
+    SELECT nextval('prix_option_seq') INTO next_val;
+    RETURN 'PRIX_OPT' || LPAD(next_val::TEXT, 4, '0');
+END;
+$$ LANGUAGE plpgsql;
