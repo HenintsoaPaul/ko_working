95c448e62347c4a540d8cc2a5075133bba9bf00f
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-10 03:58:46 +0300

    [api]-Add custom ApiResponse

---
+++ ko_working_api/src/main/java/itu/ko_working_api/dto/api/ApiResponse.java
@@ -0,0 +1,22 @@
+package itu.ko_working_api.dto.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+    private String timestamp;
+
+    public ApiResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now().toString();
+    }
+}
